20d0066d1eb7ad67c0594cceff86c455804a3d73
salah <hamidi.salah@gmail.com>
2021-07-10 14:26:59 +0200

    Authenification // Installer easyadmin

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user
+            ->setUsername('admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->encoder->encodePassword($user, 'admin2020@@'));
+
+        $user2 = new User();
+        $user2
+            ->setUsername('user')
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($this->encoder->encodePassword($user, 'user'));
+
+        $manager->persist($user);
+        $manager->persist($user2);
+
+        $manager->flush();
+    }
+}
